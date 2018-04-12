665125c8fcf7f044706ce45ca99a69b7c7db43c2
okunniga Tosin <Okunnigatosin@gmail.com>
2018-04-12 12:45:21 +0100

    HNG internship

---
+++ index.php
@@ -0,0 +1,47 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tosin's Portfolio</title>
+	<meta name="virwport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="./styles.css">
+        
+</head>
+<body>
+<div class="container">
+
+
+
+
+
+</div>
+<div class="hero-image">
+  <div class="hero-text">
+    <h1>HNG4 INTERNSHIP</h1>
+    <p id="date"> <?php
+echo "Today is " . date("l") . "<br>";
+
+?></p>
+   	<p id="time">
+   		<?php echo " ".date("h:i:sa");  ?>
+   	</p>
+    
+  </div>
+
+  </div>
+
+  
+
+
+  
+  
+
+
+
+</div>
+</body>
+</html>
